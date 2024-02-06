9f6041b89bfcba7aba9fb4c615b30498ab34bd1b
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-02-06 09:14:57 -0300

    Update exercicio7.js

--- exercicio7.js
+++ exercicio7.js
@@ -20,6 +20,8 @@ for(a=0; a<= 10; a++){
 }
 
 */
+let num = [10, 5, 8, 18];
+num.sort((a, b) => b - a); // essa condição serve para informar se A é inferior a B ( ai A fica antes de B), informar se os dois são equivalentes ou se B é inferiro a A.
+console.log(num); // Saída: [18, 10, 8, 5]
 
-let num = [10,5,8,18]
-console.log(num)
+
