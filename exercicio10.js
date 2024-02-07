28c30af22a5f72eefdedada9bd9674c5e4de4abc
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-02-06 21:13:44 -0300

    Update exercicio10.js

--- exercicio10.js
+++ exercicio10.js
@@ -1,6 +1,5 @@
-var num; 
-num = [1, 4, 5, 6, 7, 8, 9];
+var repetições;
 
-for (let a = 0; a < num.length; a++) {
-    console.log(num[a]);
-}
+repetições = [1,2,3,5,6,98,74,2,3,5,98]
+for(a=0; a <= repetições.length; a++)
+console.log(repetições[a])
