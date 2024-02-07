8f8a6b35735a99be66c909645e5ea6cf2f864935
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-02-06 23:21:37 -0300

    Create exercicio11.js

---
+++ exercicio11.js
@@ -0,0 +1,27 @@
+/*Soma dos Elementos: Escreva um programa que calcula a soma de todos os elementos em um vetor de números inteiros.
+Soma dos Elementos: Escreva um programa que calcula a soma de todos os elementos em um vetor de números inteiros.
+
+Maior e Menor Elemento: Escreva um programa que encontre o maior e o menor elemento em um vetor de números inteiros.
+
+Média dos Elementos: Escreva um programa que calcula a média dos elementos em um vetor de números inteiros.
+
+Contagem de Elementos Pares: Escreva um programa que conta quantos elementos pares existem em um vetor de números inteiros.
+
+Inversão do Vetor: Escreva um programa que inverta a ordem dos elementos em um vetor.
+
+Ordenação do Vetor: Escreva um programa que ordene os elementos em um vetor em ordem crescente ou decrescente.
+
+Remoção de Elemento: Escreva um programa que remova um elemento específico de um vetor.
+
+Intercalação de Vetores: Escreva um programa que intercale os elementos de dois vetores em um terceiro vetor.
+
+Verificação de Elemento Único: Escreva um programa que verifique se um determinado elemento existe em um vetor e, se existir, exiba a posição em que ele está.
+
+Combinação de Vetores: Escreva um programa que combine dois vetores em um terceiro vetor, alternando os elementos de cada vetor.*/
+
+var num;
+var  soma =0;
+num = [1, 2, 3, 5, 4, 7, 8, 5, 6]
+for (let i = 0; i < num.length; i++)
+soma += num[i];
+console.log(soma)
