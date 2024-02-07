899c056717214024bb13f5a0d82148bba6f925db
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-02-07 09:13:43 -0300

    Update exercicio11.js

--- exercicio11.js
+++ exercicio11.js
@@ -26,10 +26,30 @@ for (let i = 0; i < num.length; i++)
 soma += num[i];
 console.log(soma) */
 
-//topico2
-var num;
-var  soma =0; 
-num = [1, 2, 3, 5, 4, 7, 8, 5, 6]
-for (let i = 0; i < num.length; i++)
-soma += num[i];
+//topico2: Maior e Menor Elemento: Escreva um programa que encontre o maior e o menor elemento em um vetor de números inteiros.
+function encontrarPosicaoMaiorMenor(vetor) {
+    let posicaoMaior = 0;
+    let posicaoMenor = 0;
+
+    for (let i = 1; i < vetor.length; i++) {
+        if (vetor[i] > vetor[posicaoMaior]) {
+            posicaoMaior = i;
+        }
+
+        if (vetor[i] < vetor[posicaoMenor]) {
+            posicaoMenor = i;
+        }
+    }
+
+    return { posicaoMaior, posicaoMenor };
+}
+
+// Exemplo de uso
+let vetor = [10, 5, 3, 8, 15, 2, 9];
+let { posicaoMaior, posicaoMenor } = encontrarPosicaoMaiorMenor(vetor);
+
+console.log("Posição do maior valor:", posicaoMaior); // Deve retornar 4 (posição do valor 15)
+console.log("Posição do menor valor:", posicaoMenor); // Deve retornar 5 (posição do valor 2)
+
+
 
