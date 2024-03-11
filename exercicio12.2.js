04a0db798c083323a729ba513230858e0ddf3563
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-03-11 11:01:21 -0300

    Create exercicio12.2.js

---
+++ exercicio12.2.js
@@ -0,0 +1,9 @@
+console.log("Números pares de 1 a 100:");
+
+// Loop de 1 a 100
+for (let i = 1; i <= 100; i++) {
+    // Verifica se o número é par
+    if (i % 2 === 0) {
+        console.log(i); // Imprime o número par
+    }
+}
