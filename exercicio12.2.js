96d197c7744ed04f86cfe5ee8fa39c9aa0509a81
Matheus Patricio da S e Silva <matheuspatricio.aspx@gmail.com>
2024-03-11 13:41:54 -0300

    Update exercicio12.2.js

--- exercicio12.2.js
+++ exercicio12.2.js
@@ -7,3 +7,28 @@ for (let i = 1; i <= 100; i++) {
         console.log(i); // Imprime o número par
     }
 }
+
+/*
+#include <stdio.h>
+
+int main() {
+    int a, b;
+
+    // Pergunta e lê o primeiro valor inteiro
+    printf("");
+    scanf("%d", &a);
+
+    // Pergunta e lê o segundo valor inteiro
+    printf("");
+    scanf("%d", &b);
+
+    // Calcula a soma dos dois valores
+    int x = a + b;
+
+    // Imprime o resultado conforme o formato especificado
+    printf("X = %d\n", x);
+
+    return 0;
+}
+
+/*
